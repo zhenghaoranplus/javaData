7c05ed46b3a74a7d40bb9873fe702547e4fab74b
郑浩然 <921603112@qq.com>
2019-01-04 17:09:52 +0800

    Initial commit

---
+++ src/main/java/com/zhr/controller/UserController.java
@@ -0,0 +1,5 @@
+package com.zhr.controller;
+
+
+public class UserController {
+}
---
+++ src/main/java/com/zhr/entity/User.java
@@ -0,0 +1,11 @@
+package com.zhr.entity;
+
+public class User {
+
+
+
+    private int id;
+    private String phone;
+    private String sex;
+    private String password;
+}
